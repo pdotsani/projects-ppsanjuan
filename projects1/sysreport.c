fdea7f69fabb1cec56e3e6646242fb3b12cfe93f
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-03-01 11:56:37 -0800

    sysreprot bootstrap parts

--- projects1/sysreport.c
+++ projects1/sysreport.c
@@ -8,7 +8,7 @@
 
 int host_name(int proc_fd, char name[64])
 {
-  int fd = openat(proc_fd, "/etc/hostname", O_RDONLY);
+  int fd = openat(proc_fd, "./sys/kernel/hostname", O_RDONLY);
   if (fd == -1) {
     perror("openat");
     return -1;
@@ -19,6 +19,41 @@ int host_name(int proc_fd, char name[64])
   close(fd);
 
   return 0;
+}
+
+int kernel_version(int proc_fd, char name[32]) {
+  int fd = openat(proc_fd, "./sys/kernel/osrelease", O_RDONLY);
+  if (fd == -1) {
+    perror("openat");
+    return -1;
+  }
+
+  read(fd, name, 64);
+
+  close(fd);
+
+  return 0;
+}
+
+struct cpuinfo cpu_usage(int proc_fd) {
+  int fd = openat(proc_fd, "./cpuinfo", O_RDONLY);
+  if (fd == -1) {
+    perror("openat");
+  }
+  struct cpuinfo cpu;
+
+  return cpu;
+}
+
+unsigned long total_memory(int proc_fd) {
+  int fd = openat(proc_fd, "./meminfo", O_RDONLY);
+  if (fd == -1) {
+    perror("openat");
+    return -1;
+  }
+
+  // run find keyword here
+
 }
 
 int main(void)
@@ -32,7 +67,11 @@ int main(void)
   char hn[64];
   host_name(proc_fd, hn);
 
+  char kn[32];
+  kernel_version(proc_fd, kn);
+
   printf("Welcome to %s\n", hn);
+  printf("Kernel is %s\n", kn);
 
   return 0;
 }
